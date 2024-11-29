fff5e50bd00258ceaf049d95fc3319093657baca
Nareshkorlapu <nareshkorlapu123@gmail.com>
2024-11-29 15:24:34 +0530

    First day of JS

---
+++ JavaScriot/first/script.js
@@ -0,0 +1,16 @@
+/*
+This is external 
+JS- multi line
+ comments
+*/
+var a=25;
+console.log("The value is" +(a+1));
+/* try to take two variables and print its sum in console*/
+var x=10;
+var y=15;
+console.log("The sum is" +(x+y));
+var x=10,y=20;
+var c=x+y;
+console.log("The sum is" +c);
+alert("The sum is" +c);
+confirm("Are you sure?");
