9d609acdeb8fe45272372a54b0adec472b08f5c4
Nareshkorlapu <nareshkorlapu123@gmail.com>
2024-12-04 18:39:56 +0530

    hii

--- JavaScript/Code/first/script.js
+++ JavaScript/Code/first/script.js
@@ -1,16 +1,13 @@
-/*
-This is external 
+/* 
+This is External
 JS- multi line
- comments
+Comment
 */
-var a=25;
-console.log("The value is" +(a+1));
-/* try to take two variables and print its sum in console*/
-var x=10;
-var y=15;
-console.log("The sum is" +(x+y));
-var x=10,y=20;
-var c=x+y;
-console.log("The sum is" +c);
-alert("The sum is" +c);
-confirm("Are you sure?");
+var a = 25;
+console.log("The value is " + (a + 1));
+/* try to take two variables and print its sum in console */
+var x = 10, y = 20;
+var c = x + y;
+console.log(a)
+alert("The sum is " + c);
+confirm("Are you sure?")
